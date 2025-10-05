a9fb2cdf89a15994b793b08963c1b4a5ed46af3f
Thai Truong Giang <truonggiang121604@gmail.com>
2025-10-05 11:58:55 +0700

    BTTHTuan 5 TruongGiang

---
+++ Sach.java
@@ -0,0 +1,55 @@
+public class Sach{
+    private String maSach;
+    private String tieuDe;   
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong;
+
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong) 
+    {
+            this.maSach = maSach;
+            this.tieuDe = tieuDe;
+            this.tacGia = tacGia;
+            this.namXuatBan = namXuatBan;
+            this.soLuong = soLuong;
+    }
+    public Sach() {
+        this.maSach = "";
+        this.tieuDe = "";
+        this.tacGia = "";
+        this.namXuatBan = 0;
+        this.soLuong = 0;
+    }
+
+    public String getMaSach() {
+        return maSach;
+    }
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+    public String getTieuDe() {
+        return tieuDe;
+    }
+    public void setTieuDe(String tieuDe) {
+        this.tieuDe = tieuDe;
+    }
+    public String getTacGia() {
+        return tacGia;
+    }
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+    public int getNamXuatBan() {
+        return namXuatBan;
+    }
+    public void setNamXuatBan(int namXuatBan) {
+        this.namXuatBan = namXuatBan;
+    }
+    public int getSoLuong() {
+        return soLuong;
+    }
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+}
