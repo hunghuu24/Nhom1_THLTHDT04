e021d23eae771db55cc0472a57d2282900c46f66
Thai Truong Giang <truonggiang121604@gmail.com>
2025-10-13 21:56:17 +0700

    bai thuc hanh tuan 5 va tuan 6 trong packet

---
+++ BTTH_Tuan6/Sach.java
@@ -0,0 +1,63 @@
+package BTTH_Tuan6;
+public class Sach{
+    private String maSach;
+    private String tieuDe;   
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong;
+    // Constructor -- tính chất: tính đóngg gói, kế thừa, đa hình, trừu tượng.
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong) 
+    {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+    }
+    // Default constructor
+    public Sach() {
+        this.maSach = "";
+        this.tieuDe = "";
+        this.tacGia = "";
+        this.namXuatBan = 0;
+        this.soLuong = 0;
+    }
+    public void hienThiThongTin() {
+        System.out.println("Mã sách: " + maSach);
+        System.out.println("Tiêu đề: " + tieuDe);
+        System.out.println("Tác giả: " + tacGia);
+        System.out.println("Năm xuất bản: " + namXuatBan);
+        System.out.println("Số lượng: " + soLuong);
+    }
+    // Getters and Setters
+    public String getMaSach() {
+        return maSach;
+    }
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+    public String getTieuDe() {
+        return tieuDe;
+    }
+    public void setTieuDe(String tieuDe) {
+        this.tieuDe = tieuDe;
+    }
+    public String getTacGia() {
+        return tacGia;
+    }
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+    public int getNamXuatBan() {
+        return namXuatBan;
+    }
+    public void setNamXuatBan(int namXuatBan) {
+        this.namXuatBan = namXuatBan;
+    }
+    public int getSoLuong() {
+        return soLuong;
+    }
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+}
---
+++ BTTH_Tuan6/SachTieuThuyet.java
@@ -0,0 +1,35 @@
+package BTTH_Tuan6;
+public class SachTieuThuyet extends Sach {
+    private String theLoai;
+    private boolean laSachSerie;
+    // Constructor
+    public SachTieuThuyet(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, String theLoai, boolean laSachSerie) {
+        super(maSach,  tieuDe, tacGia, namXuatBan, soLuong);
+        this.theLoai = theLoai;
+        this.laSachSerie = laSachSerie;
+    }
+    // Default constructor
+    public SachTieuThuyet() {
+        super();
+        this.theLoai = "";
+        this.laSachSerie = false;
+    }   
+    // Getters and Setters
+    public String getTheLoai() {
+        return theLoai;
+    }
+    public void setTheLoai(String theLoai) {
+        this.theLoai = theLoai;
+    }
+    public boolean getlaSachSeries() {
+        return laSachSerie;
+    }
+    public void setlaSachSeries(boolean laSachSerie) {
+        this.laSachSerie = laSachSerie;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Thể loại: " + theLoai + ", Là sách series: " + (laSachSerie ? "Có" : "Không");
+    }
+}
