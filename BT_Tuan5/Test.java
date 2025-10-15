ed25152d768f530a5e86975b9aa67001598a1adb
Thái Trường GIang <truonggiang121604@gmail.com>
2025-10-15 08:05:05 +0700

    Update Test.java

--- BT_Tuan5/Test.java
+++ BT_Tuan5/Test.java
@@ -1,4 +1,4 @@
-package BTTH_TUAN5;
+package BT_TUAN5;
 public class Test {
     public static void main(String[] args) {
         Sach sach1 = new Sach("001", "Java Programming", "Nguyen Van A", 2020, 10);
